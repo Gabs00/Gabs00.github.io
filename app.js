25a4dbc1bee60193e707c8bcb0772b000b37c455
Gabs00 <gewen87@gmail.com>
2014-10-17 23:27:37 -0400

    added a few potential site areas

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 angular.module('personal', ['ui.router'])
 .config(function($stateProvider, $urlRouterProvider){
   $stateProvider
-    .state('i', {
-      url: '/index.html',
-      templateUrl: 'home.html'
+    .state('home', {
+      url:'/index',
+      templateUrl: './home.html',
+    })
+    .state('blog', {
+      url:'/blog',
+      template:'<h2>My Blogs</h2>'
+    })
+    .state('proj', {
+      url:'/projects',
+      template: '<h2>My Projects</h2>'
+    })
+    .state('stuff', {
+      url:'/other',
+      template:'<h2>stuff</h2>'
     });
 });
